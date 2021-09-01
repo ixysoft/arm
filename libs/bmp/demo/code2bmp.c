030ff2e7281929994d257daba6d6c0fe05ab8723
FrancisX <1340058590@qq.com>
2021-09-01 18:28:34 +0800

    代码转bmp

--- libs/bmp/demo/code2bmp.c
+++ libs/bmp/demo/code2bmp.c
@@ -4,6 +4,7 @@
 
 int main(int argc, char** argv){
     if(argc == 1){
+        printf("代码转bmp文件");
         printf("%s 文件名\n", argv[0]);
     }else{
         FILE* fp = fopen(argv[1], "rb");
@@ -11,26 +12,32 @@ int main(int argc, char** argv){
             perror("文件打开失败");
             return -1;
         }
-        char buf[BUFSIZ];
-        fread(buf, BUFSIZ, 1, fp);
+        char buf[BUFSIZ*32];
+        fread(buf, BUFSIZ, 32, fp);
         int length = (strlen(buf) + 1);
         int image_size = ((length / 3) + (length % 3 > 0)) * 3; //图片大小
+        image_size /= 3;
         int width = (int)sqrt(image_size);
-        int height = (image_size / width + (image_size % width > 0)) * width;
+        width = (width / 4 + (width % 4 > 0)) * 4;
+        int height = (image_size / width + (image_size % width > 0));
+        printf("width = %d, height = %d, size = %d\n", width, height, image_size);
         bitmap_file_t* bmp = bmp_create(width, height,24);
         int i;
         color_t color;
+        memset(bmp->raw_data, 0x00, width * height * 3);
         for(i = 0; i < length; i += 3){
-            color.red = i < length ? buf[i] : 0;
+            color.blue = i < length ? buf[i] : 0;
             color.green = i+1 < length ? buf[i+1] : 0;
-            color.blue = i+2 < length ? buf[i+2] : 0;
+            color.red = i+2 < length ? buf[i+2] : 0;
             int x = (i / 3) % width;
             int y = i / 3 / width;
-            bmp_set_pixel(bmp, x, y, &color);
+            bmp_set_pixel(bmp, x, height - 1 - y, &color);
         }
         fclose(fp);
 
-        bmp_save(bmp, "code.bmp");
+        char filename[256];
+        sprintf(filename, "%s.bmp", argv[1]);
+        bmp_save(bmp, filename);
     }
     return 0;
 }
